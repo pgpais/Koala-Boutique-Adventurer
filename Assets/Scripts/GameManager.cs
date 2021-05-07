1141458421cad8126ccb678430dc453dc4664780
pgpais <pgpaisdev@gmail.com>
2021-05-07 11:35:48 +0100

    Cleaned up file

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -26,18 +26,6 @@ public class GameManager : MonoBehaviour
         DontDestroyOnLoad(this);
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public void StartGame()
     {
         FirebaseCommunicator.instance.GetObject("missions", (task) =>
