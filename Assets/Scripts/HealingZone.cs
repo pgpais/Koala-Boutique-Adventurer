9ca68191c38dfe0ca097252f2b40b6d650716f96
pgpais <pgpaisdev@gmail.com>
2021-06-08 15:19:03 +0100

    Created Healing Rooms

---
+++ Assets/Scripts/HealingZone.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using MoreMountains.TopDownEngine;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class HealingZone : MonoBehaviour
+{
+    [SerializeField] int healingAmount = 1;
+    [SerializeField] float timeBetweenHeals = 0.2f;
+
+    Health targetHealth;
+    float nextHealTime;
+
+    private void Start()
+    {
+        nextHealTime = Time.time;
+    }
+
+    void HealTarget()
+    {
+        if (Time.time >= nextHealTime)
+        {
+            targetHealth.GetHealth(healingAmount, gameObject);
+            nextHealTime = Time.time + timeBetweenHeals;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (targetHealth == null)
+        {
+            targetHealth = other.GetComponent<Health>();
+        }
+
+        HealTarget();
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        HealTarget();
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        targetHealth = null;
+    }
+}
