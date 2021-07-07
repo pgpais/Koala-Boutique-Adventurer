3be6301c17218f124769f1e46e0b18fd085082f8
pgpais <pgpaisdev@gmail.com>
2021-07-07 09:26:48 +0100

    Removed sound feedback from poison damage

--- Assets/Scripts/Abilities/Health.cs
+++ Assets/Scripts/Abilities/Health.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using MoreMountains.Feedbacks;
+using MoreMountains.TopDownEngine;
 using UnityEngine;
 
 namespace Cheese
@@ -10,6 +12,8 @@ namespace Cheese
         public bool hasIronBoots = false;
         public bool hasDeflectingArmor = false;
 
+        [SerializeField] MMFeedbacks DOTFeedbacks;
+
         public override void Damage(int damage, GameObject instigator, float flickerDuration, float invincibilityDuration)
         {
             if (instigator.CompareTag("Turret Shot") && hasDeflectingArmor && Random.Range(0, 1f) < 0.5f)
@@ -18,7 +22,67 @@ namespace Cheese
             if (instigator.CompareTag("Spike Trap") && hasIronBoots)
                 return;
 
-            base.Damage(damage, instigator, flickerDuration, invincibilityDuration);
+            if (Invulnerable)
+            {
+                return;
+            }
+
+            // if we're already below zero, we do nothing and exit
+            if ((CurrentHealth <= 0) && (InitialHealth != 0))
+            {
+                return;
+            }
+
+            // we decrease the character's health by the damage
+            float previousHealth = CurrentHealth;
+            CurrentHealth -= damage;
+
+            if (OnHit != null)
+            {
+                OnHit();
+            }
+
+            if (CurrentHealth < 0)
+            {
+                CurrentHealth = 0;
+            }
+
+            // we prevent the character from colliding with Projectiles, Player and Enemies
+            if (invincibilityDuration > 0)
+            {
+                DamageDisabled();
+                StartCoroutine(DamageEnabled(invincibilityDuration));
+            }
+
+            // we trigger a damage taken event
+            MMDamageTakenEvent.Trigger(_character, instigator, CurrentHealth, damage, previousHealth);
+
+            if (_animator != null)
+            {
+                _animator.SetTrigger("Damage");
+            }
+
+            bool isDoT = invincibilityDuration == 0; // this is not a good definer, but it works
+            if (isDoT)
+            {
+                DOTFeedbacks?.PlayFeedbacks(this.transform.position);
+            }
+            else
+            {
+                DamageMMFeedbacks?.PlayFeedbacks(this.transform.position);
+            }
+
+            // we update the health bar
+            UpdateHealthBar(true);
+
+            // if health has reached zero
+            if (CurrentHealth <= 0)
+            {
+                // we set its health to zero (useful for the healthbar)
+                CurrentHealth = 0;
+
+                Kill();
+            }
         }
     }
 }
