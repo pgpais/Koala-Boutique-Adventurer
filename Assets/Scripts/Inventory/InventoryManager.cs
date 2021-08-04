96d37fe003ad1aadfbb4996a2151395b6d686e24
pgpais <pgpaisdev@gmail.com>
2021-08-04 14:24:26 +0100

    added new items log

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class InventoryManager : MonoBehaviour
 {
+    public static string newItemsReferenceName = "newItems";
     public static InventoryManager instance;
     public static UnityEvent<string, int> ItemAdded = new UnityEvent<string, int>();
     public static UnityEvent ItemsAddedToGlobalInventory = new UnityEvent();
@@ -76,10 +78,14 @@ public class InventoryManager : MonoBehaviour
             QuestManager.instance.TryToCompleteAdventurerQuest(itemQuantity);
         }
 
+        int diseasedGoldLoss = 0;
+
         if (DiseasedManager.instance != null && DiseasedManager.instance.enabled && DiseasedManager.instance.DiseasedItem != null)
         {
-            HandleDiseasedItem(DiseasedManager.instance.DiseasedItem);
+            diseasedGoldLoss = HandleDiseasedItem(DiseasedManager.instance.DiseasedItem);
         }
+
+        AddToNewItemsLog(diseasedGoldLoss);
 
         foreach (var itemName in itemQuantity.Keys)
         {
@@ -95,12 +101,61 @@ public class InventoryManager : MonoBehaviour
             itemQuantity.Remove("Gem");
         }
 
-
         ItemManager.instance.AddItemsAfterGetting(itemQuantity);
         ItemsAddedToGlobalInventory.Invoke();
     }
 
-    private void HandleDiseasedItem(Item diseasedItem)
+    private void AddToNewItemsLog(int diseasedGoldLoss)
+    {
+        NewItemsList currentNewItemsList = new NewItemsList(diseasedGoldLoss, itemQuantity, DiseasedManager.instance.DiseasedItem.ItemName);
+
+        GetNewItemsListAndAddCurrent(currentNewItemsList);
+    }
+
+    private void GetNewItemsListAndAddCurrent(NewItemsList currentNewItemsList)
+    {
+        FirebaseCommunicator.instance.GetObject(newItemsReferenceName, (task) =>
+        {
+            if (task.IsFaulted)
+            {
+                Debug.Log("Error getting new items list: " + task.Exception.InnerException.Message);
+            }
+            else
+            {
+                NewItemsList newItemsListToSend;
+                string json = task.Result.GetRawJsonValue();
+                if (string.IsNullOrEmpty(json))
+                {
+                    newItemsListToSend = currentNewItemsList;
+                }
+                else
+                {
+                    NewItemsList oldNewItemsList = JsonConvert.DeserializeObject<NewItemsList>(json);
+                    newItemsListToSend = currentNewItemsList.Merge(oldNewItemsList);
+                }
+
+                SendNewItemsList(newItemsListToSend);
+            }
+        });
+    }
+
+    private void SendNewItemsList(NewItemsList newItemsListToSend)
+    {
+        string json = JsonConvert.SerializeObject(newItemsListToSend);
+        FirebaseCommunicator.instance.SendObject(json, newItemsReferenceName, (task) =>
+        {
+            if (task.IsFaulted)
+            {
+                Debug.Log("Error sending new items list: " + task.Exception.InnerException.Message);
+            }
+            else if (task.IsCompleted)
+            {
+                Debug.Log("New items list sent!");
+            }
+        });
+    }
+
+    private int HandleDiseasedItem(Item diseasedItem)
     {
         string itemName = diseasedItem.ItemName;
         if (itemQuantity.ContainsKey(itemName))
@@ -108,12 +163,49 @@ public class InventoryManager : MonoBehaviour
             int quantity = itemQuantity[itemName];
             itemQuantity.Remove(itemName);
 
-            GoldManager.GetGoldSendWithModifier(diseaseModifier * quantity);
+            int diseasedGoldLoss = diseaseModifier * quantity;
+            GoldManager.GetGoldSendWithModifier(diseasedGoldLoss);
+            return diseasedGoldLoss;
+        }
+
+        return 0;
+    }
+
+
+
+    class NewItemsList
+    {
+        public int diseasedGoldLoss;
+        public string diseasedItemName;
+        public Dictionary<string, int> lootedItems;
+
+        public NewItemsList(int diseasedGoldLoss, Dictionary<string, int> lootedItems, string diseasedItemName)
+        {
+            this.diseasedGoldLoss = diseasedGoldLoss;
+            this.lootedItems = lootedItems;
+            this.diseasedItemName = diseasedItemName;
+        }
+
+        internal NewItemsList Merge(NewItemsList oldNewItemsList)
+        {
+            diseasedGoldLoss += oldNewItemsList.diseasedGoldLoss;
+            foreach (var lootedItem in oldNewItemsList.lootedItems)
+            {
+                if (lootedItems.ContainsKey(lootedItem.Key))
+                {
+                    lootedItems[lootedItem.Key] += lootedItem.Value;
+                }
+                else
+                {
+                    lootedItems.Add(lootedItem.Key, lootedItem.Value);
+                }
+            }
+            return this;
         }
     }
-}
 
-struct MissionStats
-{
-    //TODO: Info to log on the mission so it can be shown on the Manager's UI
+    struct MissionStats
+    {
+        //TODO: Info to log on the mission so it can be shown on the Manager's UI
+    }
 }
